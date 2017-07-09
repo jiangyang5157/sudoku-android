23e9800a41963fcacb7815c9ce9d1860f6fecdfa
yangjiang <jiangyang5157@gmail.com>
2017-07-09 20:06:03 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/data/Terminal.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/data/Terminal.kt
@@ -3,3 +3,29 @@ package com.gmail.jiangyang5157.sudoku.data
 /**
  * Created by Yang Jiang on July 09, 2017
  */
+class Terminal() {
+
+    val edge: Int = 0
+
+    override fun toString(): String {
+        return "TODO Terminal.toString"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as Terminal
+
+        // TODO
+//        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        // TODO
+//        return id?.hashCode() ?: 0
+        return 0
+    }
+}
