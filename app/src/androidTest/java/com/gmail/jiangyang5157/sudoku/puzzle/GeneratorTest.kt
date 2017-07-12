bfeab7783332e91a4a4df33cdc46c8eab5561fef
yangjiang <jiangyang5157@gmail.com>
2017-07-12 21:16:16 +1200

    na

--- app/src/androidTest/java/com/gmail/jiangyang5157/sudoku/puzzle/GeneratorTest.kt
+++ app/src/androidTest/java/com/gmail/jiangyang5157/sudoku/puzzle/GeneratorTest.kt
@@ -14,11 +14,11 @@ class GeneratorTest {
 
     @Test
     fun test_generator() {
-        val tBytes = Sudoku.genByte(9, 0, 4, 55)
+        val tBytes = Sudoku.genByte(9, 4, 55)
         Assert.assertNotNull(tBytes)
         Assert.assertTrue(tBytes.isNotEmpty())
         Assert.assertNotEquals("null", tBytes)
-        val tString = Sudoku.genString(9, 0, 4, 55)
+        val tString = Sudoku.genString(9, 4, 55)
         Assert.assertNotNull(tString)
         Assert.assertTrue(tString.isNotEmpty())
         Assert.assertNotEquals("null", tString)
