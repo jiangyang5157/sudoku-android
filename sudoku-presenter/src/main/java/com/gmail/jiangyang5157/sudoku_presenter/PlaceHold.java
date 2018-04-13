b77b3c6a47b52db7101ef08245d1bcefbc94bd1e
yangjiang <jiangyang5157@gmail.com>
2018-04-13 23:35:16 +1200

    na

--- sudoku-presenter/src/main/java/com/gmail/jiangyang5157/sudoku_presenter/PlaceHold.java
+++ sudoku-presenter/src/main/java/com/gmail/jiangyang5157/sudoku_presenter/PlaceHold.java
@@ -4,4 +4,8 @@ package com.gmail.jiangyang5157.sudoku_presenter;
  * Created by Yang Jiang on April 13, 2018
  */
 public class PlaceHold {
+
+    public void asd() {
+        AppInjector sd;
+    }
 }
