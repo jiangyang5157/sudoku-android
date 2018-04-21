3131cee816efbf6cb3408100078db1ffaaef54cf
yangjiang <jiangyang5157@gmail.com>
2018-04-21 20:29:44 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/TerminalView.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/TerminalView.kt
@@ -3,7 +3,7 @@ package com.gmail.jiangyang5157.sudoku.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
-import com.gmail.jiangyang5157.kotlin_android_kit.view.RenderView
+import com.gmail.jiangyang5157.kotlin_android_kit.widget.RenderView
 
 /**
  * Created by Yang Jiang on July 18, 2017
