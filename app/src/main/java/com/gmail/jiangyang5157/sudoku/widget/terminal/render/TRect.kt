241dbb93dcf138183c15d2679bb47b9c8c0780c2
yangjiang <jiangyang5157@gmail.com>
2018-04-25 13:52:46 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/terminal/render/TRect.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/terminal/render/TRect.kt
@@ -8,7 +8,7 @@ import com.gmail.jiangyang5157.kotlin_kit.render.scene.Particle
  * Created by Yang Jiang on April 25, 2018
  */
 open class TRect(override var position: Vector2i = Vector2i(),
-                 override val priority: Int = 0)
+                 override var priority: Int = 0)
     : Particle<Vector2i>, TRenderable {
 
 
--- app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/terminal/render/TRenderable.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/terminal/render/TRenderable.kt
@@ -7,5 +7,5 @@ import com.gmail.jiangyang5157.kotlin_kit.render.Renderable
  * Created by Yang Jiang on April 25, 2018
  */
 interface TRenderable : Renderable<Canvas> {
-    val priority: Int
+    var priority: Int
 }
