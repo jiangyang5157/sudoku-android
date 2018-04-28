943eba706c5f742fb2144c07a45a1e9bfdce3886
yangjiang <jiangyang5157@gmail.com>
2018-04-28 21:05:15 +1200

    integrate opencv - remove ABI armeabi, mips, mips64 support

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -5,10 +5,7 @@ LOCAL_MODULE := app
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
 	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/arm64-v8a/libopencv_java3.so \
-	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/armeabi/libopencv_java3.so \
 	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/armeabi-v7a/libopencv_java3.so \
-	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/mips/libopencv_java3.so \
-	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/mips64/libopencv_java3.so \
 	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/x86/libopencv_java3.so \
 	/Users/yangjiang/Documents/workspace/android/android-sudoku/app/src/main/jniLibs/x86_64/libopencv_java3.so \
 
