8aa8d3e500e5fdd13c5b29dd497c1e70c98f4cb9
yangjiang <jiangyang5157@gmail.com>
2018-04-28 21:32:05 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/MainActivity.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/MainActivity.kt
@@ -2,25 +2,11 @@ package com.gmail.jiangyang5157.sudoku.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.gmail.jiangyang5157.kotlin_android_kit.ext.instance
 import com.gmail.jiangyang5157.kotlin_android_kit.ext.replaceFragmentInActivity
 import com.gmail.jiangyang5157.sudoku.R
-import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        const val TAG = "MainActivity"
-
-        init {
-            if (OpenCVLoader.initDebug()) {
-                Log.d(TAG, "Initialization of OpenCV was successful")
-            } else {
-                Log.d(TAG, "Initialization of OpenCV was failed")
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanActivity.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanActivity.kt
@@ -2,14 +2,28 @@ package com.gmail.jiangyang5157.sudoku.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.gmail.jiangyang5157.kotlin_android_kit.ext.instance
 import com.gmail.jiangyang5157.kotlin_android_kit.ext.replaceFragmentInActivity
 import com.gmail.jiangyang5157.sudoku.R
+import org.opencv.android.OpenCVLoader
 
 /**
  * Created by Yang Jiang on April 21, 2018
  */
 class ScanActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "ScanActivity"
+
+        init {
+            if (OpenCVLoader.initDebug()) {
+                Log.d(TAG, "Initialization of OpenCV was successful")
+            } else {
+                Log.d(TAG, "Initialization of OpenCV was failed")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
