5a43209f6fee0b099628c927d2db65f725c8d754
yangjiang <jiangyang5157@gmail.com>
2018-04-28 23:09:09 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
@@ -1,22 +1,29 @@
 package com.gmail.jiangyang5157.sudoku.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import com.gmail.jiangyang5157.sudoku.R
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
+
 
 /**
  * Created by Yang Jiang on April 21, 2018
  */
 class ScanFragment : Fragment() {
 
-    private lateinit var debugLinearLayout: LinearLayout
     private lateinit var debugScrollView: ScrollView
+    private lateinit var debugLinearLayout: LinearLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_scan, container, false)
@@ -28,16 +35,46 @@ class ScanFragment : Fragment() {
         view?.apply {
             debugScrollView = findViewById(R.id.debug_scrollview) as ScrollView
             debugLinearLayout = findViewById(R.id.debug_linearlayout) as LinearLayout
-            findViewById(R.id.debug_btn_next).setOnClickListener(debugBtnNextOnClicklistener)
+
+            debug("debug_puzzle.png")
         }
     }
 
-    private val debugBtnNextOnClicklistener = View.OnClickListener {
+    private fun debug(filename: String) {
+
+        val srcBm: Bitmap
+        val dstBm: Bitmap
+        val srcMat: Mat
+        val dstMat: Mat
+
+        /** 1. **/
         debugLinearLayout.addView(TextView(context).apply {
-            text = "testing"
+            text = "original image..."
+        })
+        srcBm = BitmapFactory.decodeStream(context.assets.open(filename))
+        debugLinearLayout.addView(ImageView(context).apply {
+            setImageBitmap(srcBm)
+        })
+        debugScrollView.fullScroll(View.FOCUS_DOWN)
+
+        /** 2. **/
+        debugLinearLayout.addView(TextView(context).apply {
+            text = "Gray scaled..."
         })
 
+        srcMat = Mat()
+        dstMat = Mat()
+        Utils.bitmapToMat(srcBm, srcMat)
+        Imgproc.cvtColor(srcMat, dstMat, Imgproc.COLOR_BGR2GRAY)
+        dstBm = Bitmap.createBitmap(dstMat.cols(), dstMat.rows(), Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(dstMat, dstBm)
+
+        debugLinearLayout.addView(ImageView(context).apply {
+            setImageBitmap(dstBm)
+        })
         debugScrollView.fullScroll(View.FOCUS_DOWN)
+
+
     }
 
 }
