1c304fa2e35380f73ead6b0a0456820708dfc692
yangjiang <jiangyang5157@gmail.com>
2018-04-29 19:19:56 +1200

    JavaCamera2View lacks of framerate, create ScanCamera2View, todo: opt

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
@@ -1,34 +1,26 @@
 package com.gmail.jiangyang5157.sudoku.ui
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ScrollView
-import android.widget.TextView
 import com.gmail.jiangyang5157.sudoku.R
-import org.opencv.android.Utils
+import com.gmail.jiangyang5157.sudoku.widget.ScanCamera2View
+import org.opencv.android.CameraBridgeViewBase
 import org.opencv.core.Mat
-import org.opencv.core.MatOfPoint
-import org.opencv.core.Scalar
-import org.opencv.core.Size
-import org.opencv.imgproc.Imgproc
-import org.opencv.imgproc.Imgproc.ADAPTIVE_THRESH_MEAN_C
-import org.opencv.imgproc.Imgproc.THRESH_BINARY_INV
-
 
 /**
  * Created by Yang Jiang on April 21, 2018
  */
-class ScanFragment : Fragment() {
+class ScanFragment : Fragment(), CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private lateinit var debugScrollView: ScrollView
-    private lateinit var debugLinearLayout: LinearLayout
+    companion object {
+        const val TAG = "ScanFragment"
+    }
+
+    private lateinit var mScanCamera2View: ScanCamera2View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_scan, container, false)
@@ -38,57 +30,83 @@ class ScanFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view?.apply {
-            debugScrollView = findViewById(R.id.debug_scrollview) as ScrollView
-            debugLinearLayout = findViewById(R.id.debug_linearlayout) as LinearLayout
-
-            debug("puzzle_paper.jpg")
-            debug("puzzle_rotation.jpg")
-            debug("puzzle_rotated_curve.jpg")
+            mScanCamera2View = findViewById(R.id.scancamera2view) as ScanCamera2View
+            mScanCamera2View.enableFpsMeter()
+            mScanCamera2View.enableView()
+            mScanCamera2View.setCvCameraViewListener(this@ScanFragment)
         }
     }
 
-    private fun debug(filename: String) {
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        Log.d(TAG, "onCameraViewStarted: $width x $height")
+    }
 
-        /**
-         *
-         */
-        val srcBm = BitmapFactory.decodeStream(context.assets.open(filename))
-        val srcMat = Mat()
-        Utils.bitmapToMat(srcBm, srcMat, false)
-        debugShowInfo("Original image", srcMat)
+    override fun onCameraViewStopped() {
+        Log.d(TAG, "onCameraViewStopped")
+    }
 
-        /**
-         * We don't want to bother with the colour information, so just skip it
-         */
-        val grayScaledMat = Mat()
-        Imgproc.cvtColor(srcMat, grayScaledMat, Imgproc.COLOR_BGR2GRAY)
-        debugShowInfo("Gray scaled, BGR2GRAY", null)
+    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
+        Log.d(TAG, "inputFrame=" + inputFrame.toString())
+        return inputFrame.rgba()
+    }
 
-        /**
-         * This smooths out the noise a bit and makes extracting the grid lines easier
-         */
-        val blurMat = Mat()
-        Imgproc.GaussianBlur(grayScaledMat, blurMat,
-                Size(5.0, 5.0), 0.0, 0.0)
-        debugShowInfo("GaussianBlur: (5, 5), (0, 0)", null)
 
-        /**
-         * It calculates a mean over a 5x5 window and subtracts 2 from the mean.
-         * This is the threshold level for every pixel.
-         */
-        val adaptiveThresholdMat = Mat()
-        Imgproc.adaptiveThreshold(blurMat, adaptiveThresholdMat,
-                255.0, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 5, 2.0)
-        debugShowInfo("AdaptiveThreshold: 255, MEAN_C, BINARY_INV, 5, 2", adaptiveThresholdMat)
-
-        /**
-         * Thresholding operation can disconnect certain connected parts (like lines).
-         * So dilating the image once will fill up any small "cracks" that might have crept in.
-         *
-         * 0.0, 1.0, 0.0,
-         * 1.0, 1.0, 1.0,
-         * 0.0, 1.0, 0.0
-         */
+//    private lateinit var debugScrollView: ScrollView
+//    private lateinit var debugLinearLayout: LinearLayout
+//
+//    private fun debugOnViewCreated(view: View?) {
+//        view?.apply {
+//            debugScrollView = findViewById(R.id.debug_scrollview) as ScrollView
+//            debugLinearLayout = findViewById(R.id.debug_linearlayout) as LinearLayout
+//
+//            debug("puzzle_paper.jpg")
+//            debug("puzzle_rotation.jpg")
+//            debug("puzzle_rotated_curve.jpg")
+//        }
+//    }
+//
+//    private fun debug(filename: String) {
+//
+//        /**
+//         *
+//         */
+//        val srcBm = BitmapFactory.decodeStream(context.assets.open(filename))
+//        val srcMat = Mat()
+//        Utils.bitmapToMat(srcBm, srcMat, false)
+//        debugShowInfo("Original image", srcMat)
+//
+//        /**
+//         * We don't want to bother with the colour information, so just skip it
+//         */
+//        val grayScaledMat = Mat()
+//        Imgproc.cvtColor(srcMat, grayScaledMat, Imgproc.COLOR_BGR2GRAY)
+//        debugShowInfo("Gray scaled, BGR2GRAY", null)
+//
+//        /**
+//         * This smooths out the noise a bit and makes extracting the grid lines easier
+//         */
+//        val blurMat = Mat()
+//        Imgproc.GaussianBlur(grayScaledMat, blurMat,
+//                Size(5.0, 5.0), 0.0, 0.0)
+//        debugShowInfo("GaussianBlur: (5, 5), (0, 0)", null)
+//
+//        /**
+//         * It calculates a mean over a 5x5 window and subtracts 2 from the mean.
+//         * This is the threshold level for every pixel.
+//         */
+//        val adaptiveThresholdMat = Mat()
+//        Imgproc.adaptiveThreshold(blurMat, adaptiveThresholdMat,
+//                255.0, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 5, 2.0)
+//        debugShowInfo("AdaptiveThreshold: 255, MEAN_C, BINARY_INV, 5, 2", adaptiveThresholdMat)
+//
+//        /**
+//         * Thresholding operation can disconnect certain connected parts (like lines).
+//         * So dilating the image once will fill up any small "cracks" that might have crept in.
+//         *
+//         * 0.0, 1.0, 0.0,
+//         * 1.0, 1.0, 1.0,
+//         * 0.0, 1.0, 0.0
+//         */
 //        val dilateMat = Mat()
 //        val dilationSize = 0.5
 //        val kernelMat = Imgproc.getStructuringElement(
@@ -97,47 +115,45 @@ class ScanFragment : Fragment() {
 //                Point(dilationSize, dilationSize))
 //        Imgproc.dilate(adaptiveThresholdMat, dilateMat, kernelMat)
 //        debugShowInfo("Dilate: MORPH_CROSS, 0.5", dilateMat)
-
-        /**
-         *
-         */
-        val contoursMatOfPoint = arrayListOf<MatOfPoint>()
-        Imgproc.findContours(adaptiveThresholdMat, contoursMatOfPoint, Mat(),
-                Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
-        val contourMat = adaptiveThresholdMat.clone()
-        Imgproc.cvtColor(contourMat, contourMat, Imgproc.COLOR_GRAY2BGR)
-        var maxContourIndex = -1
-        var maxContourArea = 0.0
-        contoursMatOfPoint.forEachIndexed { i, matOfPoint ->
-            Imgproc.contourArea(matOfPoint).apply {
-                if (this > maxContourArea) {
-                    maxContourArea = this
-                    maxContourIndex = i
-                }
-            }
-        }
-        Imgproc.drawContours(contourMat, contoursMatOfPoint, maxContourIndex,
-                Scalar(255.0, 0.0, 0.0), 2)
-        debugShowInfo("DrawContours: RETR_EXTERNAL, APPROX_SIMPLE", contourMat)
-
-
-    }
-
-    private fun debugShowInfo(title: String, mat: Mat?) {
-        debugLinearLayout.addView(TextView(context).apply {
-            text = title
-        })
-        mat?.apply {
-            debugLinearLayout.addView(ImageView(context).apply {
-                setImageBitmap(Bitmap.createBitmap(
-                        mat.cols(), mat.rows(), Bitmap.Config.RGB_565).apply {
-                    Utils.matToBitmap(mat, this, false)
-                })
-            }, LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    600
-            ))
-        }
-    }
+//
+//        /**
+//         *
+//         */
+//        val contoursMatOfPoint = arrayListOf<MatOfPoint>()
+//        Imgproc.findContours(dilateMat, contoursMatOfPoint, Mat(),
+//                Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
+//        val contourMat = dilateMat.clone()
+//        Imgproc.cvtColor(contourMat, contourMat, Imgproc.COLOR_GRAY2BGR)
+//        var maxContourIndex = -1
+//        var maxContourArea = 0.0
+//        contoursMatOfPoint.forEachIndexed { i, matOfPoint ->
+//            Imgproc.contourArea(matOfPoint).apply {
+//                if (this > maxContourArea) {
+//                    maxContourArea = this
+//                    maxContourIndex = i
+//                }
+//            }
+//        }
+//        Imgproc.drawContours(contourMat, contoursMatOfPoint, maxContourIndex,
+//                Scalar(255.0, 0.0, 0.0), 2)
+//        debugShowInfo("DrawContours: RETR_EXTERNAL, APPROX_SIMPLE", contourMat)
+//    }
+//
+//    private fun debugShowInfo(title: String, mat: Mat?) {
+//        debugLinearLayout.addView(TextView(context).apply {
+//            text = title
+//        })
+//        mat?.apply {
+//            debugLinearLayout.addView(ImageView(context).apply {
+//                setImageBitmap(Bitmap.createBitmap(
+//                        mat.cols(), mat.rows(), Bitmap.Config.RGB_565).apply {
+//                    Utils.matToBitmap(mat, this, false)
+//                })
+//            }, LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.MATCH_PARENT,
+//                    600
+//            ))
+//        }
+//    }
 
 }
