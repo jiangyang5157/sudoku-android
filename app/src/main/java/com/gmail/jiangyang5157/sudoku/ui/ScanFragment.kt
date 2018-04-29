3d9d96e4ce03a462a6fb2adcc7c04d587b355d95
yangjiang <jiangyang5157@gmail.com>
2018-04-29 16:15:59 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
@@ -38,7 +38,10 @@ class ScanFragment : Fragment() {
             debugScrollView = findViewById(R.id.debug_scrollview) as ScrollView
             debugLinearLayout = findViewById(R.id.debug_linearlayout) as LinearLayout
 
-            debug("puzzle_2.png")
+            debug("puzzle_curving.jpg")
+            debug("puzzle_gradient.jpg")
+            debug("puzzle_newspaper.jpg")
+            debug("puzzle_regular.png")
         }
     }
 
@@ -47,46 +50,34 @@ class ScanFragment : Fragment() {
         /**
          *
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "Original image"
-        })
         val srcBm = BitmapFactory.decodeStream(context.assets.open(filename))
         val srcMat = Mat()
         Utils.bitmapToMat(srcBm, srcMat, false)
-        debugDisplayNewMat(srcMat)
+        debugDisplayNewMat("Original image", srcMat)
 
         /**
          * We don't want to bother with the colour information, so just skip it
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "Gray scaled, BGR2GRAY"
-        })
         val grayScaledMat = Mat()
         Imgproc.cvtColor(srcMat, grayScaledMat, Imgproc.COLOR_BGR2GRAY)
-        debugDisplayNewMat(grayScaledMat)
+//        debugDisplayNewMat("Gray scaled, BGR2GRAY", grayScaledMat)
 
         /**
          * This smooths out the noise a bit and makes extracting the grid lines easier
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "GaussianBlur: (5, 5), (0, 0)"
-        })
         val blurMat = Mat()
         Imgproc.GaussianBlur(grayScaledMat, blurMat,
                 Size(5.0, 5.0), 0.0, 0.0)
-        debugDisplayNewMat(blurMat)
+//        debugDisplayNewMat("GaussianBlur: (5, 5), (0, 0)", blurMat)
 
         /**
          * It calculates a mean over a 5x5 window and subtracts 2 from the mean.
          * This is the threshold level for every pixel.
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "AdaptiveThreshold: 255, MEAN_C, BINARY_INV, 5, 2"
-        })
         val adaptiveThresholdMat = Mat()
         Imgproc.adaptiveThreshold(blurMat, adaptiveThresholdMat,
                 255.0, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 5, 2.0)
-        debugDisplayNewMat(adaptiveThresholdMat)
+        debugDisplayNewMat("AdaptiveThreshold: 255, MEAN_C, BINARY_INV, 5, 2", adaptiveThresholdMat)
 
         /**
          * Thresholding operation can disconnect certain connected parts (like lines).
@@ -96,9 +87,6 @@ class ScanFragment : Fragment() {
          * 1.0, 1.0, 1.0,
          * 0.0, 1.0, 0.0
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "Dilate: MORPH_CROSS, 0.5"
-        })
         val dilateMat = Mat()
         val dilationSize = 0.5
         val kernelMat = Imgproc.getStructuringElement(
@@ -106,42 +94,38 @@ class ScanFragment : Fragment() {
                 Size(2 * dilationSize + 1, 2 * dilationSize + 1),
                 Point(dilationSize, dilationSize))
         Imgproc.dilate(adaptiveThresholdMat, dilateMat, kernelMat)
-        debugDisplayNewMat(dilateMat)
+        debugDisplayNewMat("Dilate: MORPH_CROSS, 0.5", dilateMat)
 
         /**
          *
          */
-        debugLinearLayout.addView(TextView(context).apply {
-            text = "DrawContours (largest 2): RETR_EXTERNAL, APPROX_SIMPLE"
-        })
         val contoursMatOfPoint = arrayListOf<MatOfPoint>()
         Imgproc.findContours(dilateMat, contoursMatOfPoint, Mat(),
                 Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
+        val contourMat = dilateMat.clone()
+        Imgproc.cvtColor(contourMat, contourMat, Imgproc.COLOR_GRAY2BGR)
         var maxContourIndex = -1
-        var maxContourIndex2 = -1
         var maxContourArea = 0.0
         contoursMatOfPoint.forEachIndexed { i, matOfPoint ->
             Imgproc.contourArea(matOfPoint).apply {
                 if (this > maxContourArea) {
                     maxContourArea = this
-                    maxContourIndex2 = maxContourIndex
                     maxContourIndex = i
                 }
             }
         }
-        val contourMat = dilateMat.clone()
-        Imgproc.cvtColor(contourMat, contourMat, Imgproc.COLOR_GRAY2BGR)
         Imgproc.drawContours(contourMat, contoursMatOfPoint, maxContourIndex,
-                Scalar(0.0, 0.0, 255.0), 2)
-        Imgproc.drawContours(contourMat, contoursMatOfPoint, maxContourIndex2,
                 Scalar(255.0, 0.0, 0.0), 2)
-        debugDisplayNewMat(contourMat)
+        debugDisplayNewMat("DrawContours: RETR_EXTERNAL, APPROX_SIMPLE", contourMat)
 
 
 
     }
 
-    private fun debugDisplayNewMat(mat: Mat) {
+    private fun debugDisplayNewMat(title: String, mat: Mat) {
+        debugLinearLayout.addView(TextView(context).apply {
+            text = title
+        })
         debugLinearLayout.addView(ImageView(context).apply {
             setImageBitmap(Bitmap.createBitmap(
                     mat.cols(), mat.rows(), Bitmap.Config.RGB_565).apply {
