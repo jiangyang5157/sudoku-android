93d5694ee0a4833058253fdb4c88dc41d5d13def
Yang Jiang <yang.jiang@fiserv.com>
2018-04-30 12:32:05 +1200

    tmp

---
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/widget/scan/ImgprocUtils.java
@@ -0,0 +1,5 @@
+// https://github.com/ErnestWong/SudokuSolver
+//
+// https://github.com/prajwalkr/SnapSudoku
+//
+// https://stackoverflow.com/questions/9413216/simple-digit-recognition-ocr-in-opencv-python/9620295#9620295
