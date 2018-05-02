91c080bac8f874aa8242165786ce263f98577668
yangjiang <jiangyang5157@gmail.com>
2018-05-03 01:13:24 +1200

    na

--- app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
+++ app/src/main/java/com/gmail/jiangyang5157/sudoku/ui/ScanFragment.kt
@@ -9,13 +9,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+import com.gmail.jiangyang5157.kotlin_android_kit.ext.layoutInflater
 import com.gmail.jiangyang5157.kotlin_kit.render.FPSValidation
 import com.gmail.jiangyang5157.sudoku.R
 import com.gmail.jiangyang5157.sudoku.widget.scan.Camera2CvView
 import com.gmail.jiangyang5157.sudoku.widget.scan.Camera2CvViewBase
 import com.gmail.jiangyang5157.sudoku.widget.scan.imgproc.*
-import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
@@ -41,6 +44,14 @@ class ScanFragment : Fragment(), Camera2CvViewBase.Camera2CvViewListener {
     private var width = 0
     private var height = 0
 
+    private val mGaussianBlur = GaussianBlur(5.0, 5.0, 0.0, 0.0)
+    private val mAdaptiveThreshold = AdaptiveThreshold(255.0, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 2.0)
+    private val mCrossDilate = CrossDilate(0.5)
+    private val mCanny = Canny(127.0, 255.0, 3)
+    private val mFindContours = FindContours(Imgproc.RETR_EXTERNAL)
+    private val mMaxAreaContourFilter = MaxAreaContourFilter
+    private val mDrawContour = DrawContour(Color.RED, 2)
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_scan, container, false)
         scalarAccent = color2scalar(R.color.colorAccent)
@@ -55,6 +66,8 @@ class ScanFragment : Fragment(), Camera2CvViewBase.Camera2CvViewListener {
             mCamera2CvView = findViewById(R.id.view_camera2cv) as Camera2CvView
             mCamera2CvView?.setCvCameraViewListener(this@ScanFragment)
             findViewById(R.id.btn_scan).setOnClickListener { scanToggle() }
+
+            setup_debug_panel(view)
         }
     }
 
@@ -62,8 +75,6 @@ class ScanFragment : Fragment(), Camera2CvViewBase.Camera2CvViewListener {
         Log.d(TAG, "onCameraViewStarted: ${width}x$height")
         this.width = width
         this.height = height
-        mRgba = Mat(height, width, CvType.CV_8UC4)
-        mGray = Mat(height, width, CvType.CV_8UC1)
     }
 
     override fun onCameraViewStopped() {
@@ -78,42 +89,48 @@ class ScanFragment : Fragment(), Camera2CvViewBase.Camera2CvViewListener {
         Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGB2BGR)
 
         if (mProcessorFps.accept()) {
-            handleFrame(mGray!!)
+            mGray = frameproc(mGray!!)
         }
 
         return mGray!!
     }
 
-    private val mGaussianBlur = GaussianBlur(5.0, 5.0, 0.0, 0.0)
-    private val mAdaptiveThreshold = AdaptiveThreshold(255.0, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 2.0)
-    private val mCrossDilate = CrossDilate(0.5)
-    private val mCanny = Canny(127.0, 255.0, 3)
-    private val mDrawContour = DrawContour(Color.RED, 2)
-    private val mFindContours = FindContours(Imgproc.RETR_EXTERNAL)
-    private val mMaxAreaContourFilter = MaxAreaContourFilter
+    private fun frameproc(gray: Mat): Mat {
+        var last = gray
 
-    private fun handleFrame(gray: Mat): Mat {
-        val blurMat = mGaussianBlur.convert(gray)
+        if (debug_enable_GaussianBlur) {
+            val gaussianBlurMat = mGaussianBlur.convert(last)
+            last.release()
+            last = gaussianBlurMat
+        }
 
-        val adaptiveThresholdMat = mAdaptiveThreshold.convert(blurMat)
-        blurMat.release()
+        if (debug_enable_AdaptiveThreshold) {
+            val adaptiveThresholdMat = mAdaptiveThreshold.convert(last)
+            last.release()
+            last = adaptiveThresholdMat
+        }
 
-        val dilateMat = mCrossDilate.convert(adaptiveThresholdMat)
-        adaptiveThresholdMat.release()
+        if (debug_enable_CrossDilate) {
+            val crossDilateMat = mCrossDilate.convert(last)
+            last.release()
+            last = crossDilateMat
+        }
 
-//        val canny = mCanny.convert(dilateMat)
-//        dilateMat.release()
+        if (debug_enable_Canny) {
+            val cannyMat = mCanny.convert(last)
+            last.release()
+            last = cannyMat
+        }
 
-        val contours = mFindContours.find(dilateMat)
-        dilateMat.release()
+        val contours = mFindContours.find(last)
 
         val maxAreaContour = mMaxAreaContourFilter.filter(contours)
         maxAreaContour?.apply {
-            mDrawContour.draw(gray, maxAreaContour)
+            mDrawContour.draw(last, maxAreaContour)
         }
         contours.forEach { it.release() }
 
-        return gray
+        return last
     }
 
     private fun color2scalar(@ColorRes resId: Int): Scalar {
@@ -176,6 +193,43 @@ class ScanFragment : Fragment(), Camera2CvViewBase.Camera2CvViewListener {
     override fun onPause() {
         super.onPause()
         disableScanCamera2View()
+    }
+
+    /* ################ DEBUG ################ */
+    private var debug_enable_GaussianBlur = false
+    private var debug_enable_AdaptiveThreshold = false
+    private var debug_enable_CrossDilate = false
+    private var debug_enable_Canny = false
+
+    private fun setup_debug_panel(view: View) {
+        val debug_panel_container = view.findViewById(R.id.debug_panel_container) as ViewGroup
+        val debug_panel_gaussian_blur = context.layoutInflater.inflate(R.layout.debug_panel_gaussian_blur, debug_panel_container, false)
+
+        view.findViewById(R.id.debug_btn_gaussian_blur).setOnClickListener {
+            if (debug_panel_gaussian_blur.parent == null) {
+                debug_panel_container.removeAllViews()
+                debug_panel_container.addView(debug_panel_gaussian_blur)
+            } else {
+                debug_panel_container.removeAllViews()
+            }
+        }
+
+        /* Gaussian Blur */
+        val debug_cb_guassian_blur = debug_panel_gaussian_blur.findViewById(R.id.debug_cb_guassian_blur) as CheckBox
+        val debug_tv_guassion_blur_ksize = debug_panel_gaussian_blur.findViewById(R.id.debug_tv_guassion_blur_ksize) as TextView
+        val debug_tv_guassion_blur_sigma = debug_panel_gaussian_blur.findViewById(R.id.debug_tv_guassion_blur_sigma) as TextView
+        val debug_sb_guassion_blur_ksize = debug_panel_gaussian_blur.findViewById(R.id.debug_sb_guassion_blur_sigma) as SeekBar
+        val debug_sb_guassion_blur_sigma = debug_panel_gaussian_blur.findViewById(R.id.debug_sb_guassion_blur_sigma) as SeekBar
+        debug_cb_guassian_blur.setOnCheckedChangeListener { _, isChecked ->
+            debug_enable_GaussianBlur = isChecked
+        }
+        debug_sb_guassion_blur_ksize.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+
     }
 
 }
